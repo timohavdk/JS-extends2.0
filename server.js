26d8faf2729a2a59e4e9396109256a3b570dd875
timohavdk <timoha200120@gmail.com>
2023-09-27 21:32:55 +1000

    Class Object prototype Global

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const server = express();
 
 const HOST = 4000;
 
+global.appConfig = {
+	host: HOST,
+}
+
 server.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(HOST, () => {
-	console.log(`Running on: http://localhost:${HOST}/`);
+	console.log(`Running on: http://localhost:${ global.appConfig.host }/`);
 });
